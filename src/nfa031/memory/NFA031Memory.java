ad993eef866f51b28719823980446039369507c0
FarmVivi <farmvividev@gmail.com>
2021-12-30 17:08:55 +0100

    init

---
+++ src/nfa031/memory/NFA031Memory.java
@@ -0,0 +1,16 @@
+package nfa031.memory;
+
+/**
+ *
+ * @author Victor VAIZAND <victor.vaizand.auditeur@lecnam.net>
+ */
+public class NFA031Memory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
