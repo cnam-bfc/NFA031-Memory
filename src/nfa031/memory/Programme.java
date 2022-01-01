1f2f4cbc16e25064c84914ebcbc27f9fce29c194
FarmVivi <farmvividev@gmail.com>
2022-01-01 14:58:02 +0100

    Le texte est correctement lu en éliminant les doublons

--- src/nfa031/memory/Programme.java
+++ src/nfa031/memory/Programme.java
@@ -5,61 +5,19 @@ import java.io.IOException;
 public class Programme {
 
     public static void main(String[] args) {
-        for (String str : lireTexte(".\\src\\nfa031\\memory\\Ressources\\Extrait_texte.txt")) {
+        String[] mots = lireTexte(".\\src\\nfa031\\memory\\Ressources\\Extrait_texte.txt");
+        for (String str : mots) {
             System.out.println(str);
         }
+        System.out.println("Taille: " + mots.length);
     }
 
-    // Retourne un tableau des mots d'un fichier texte sans doublons, sans
-    // ponctuation, sans espaces et sans lettres seules 
-    // (exemple pour "c'était", seulement "était" est ajouté dans le tableau)
+    // Retourne un tableau des mots d'un fichier texte
     // Retourne un tableau de 0 valeurs si une exception est levée
     static String[] lireTexte(String cheminDuFichier) {
         try {
-            String[] mots = new String[0];
-            // On lit le texte et on le met directement en minuscules
-            String texte = Lecteur.LireTexte(cheminDuFichier).toLowerCase();
-            // On concatène les caractères les uns à la suite des autre dans cette chaine de caractères
-            String temp = "";
-            // On parcours le texte caractère par caractère
-            for (int i = 0; i < texte.length(); i++) {
-                char c = texte.charAt(i);
-                // On enlève les accents des lettres et on concatène le caractère seulement si c'est une lettre
-                // On traite aussi les 2 caractères spéciaux "æ" et "œ"
-                // Source: https://fr.wikipedia.org/wiki/Diacritiques_utilis%C3%A9s_en_fran%C3%A7ais#:~:text=%C2%AB%20D%C3%A8s%20lors%20les%20voyelles%20et,%2D%20%C3%BC%20%2D%20%C3%BF%20%2D%20%C3%A7.
-                switch (c) {
-                    case 'à', 'â', 'ä' ->
-                        c = 'a';
-                    case 'é', 'è', 'ê', 'ë' ->
-                        c = 'e';
-                    case 'î', 'ï' ->
-                        c = 'i';
-                    case 'ô', 'ö' ->
-                        c = 'o';
-                    case 'ù', 'û', 'ü' ->
-                        c = 'u';
-                    case 'ÿ' ->
-                        c = 'y';
-                    case 'ç' ->
-                        c = 'c';
-                }
-                if (c >= 'a' && c <= 'z') {
-                    temp += c;
-                } else if (c == 'æ') {
-                    temp += "ae";
-                } else if (c == 'œ') {
-                    temp += "oe";
-                } else {
-                    // On ajoute le mot seulement si ce n'est pas une lettre seule
-                    // (exemple: le mot "c" ne sera pas ajouté dans le tableau puisque c'est une lettre seule)
-                    if (temp.length() > 1) {
-                        mots = ajouterPlaceTableau(mots);
-                        mots[mots.length - 1] = temp;
-                    }
-                    temp = "";
-                }
-            }
-            return mots;
+            String texte = Lecteur.LireTexte(cheminDuFichier);
+            return transformerTexteEnMots(texte);
         } catch (IOException err1) {
             System.out.println("Erreur lors de la lecture du fichier");
             // On retourne un tableau vide puisqu'une exception a été levée
@@ -67,6 +25,68 @@ public class Programme {
         }
     }
 
+    // Retourne un tableau de mots à partir d'une chaine de caractères
+    // sans doublons, sans ponctuation, sans espaces et sans lettres seules
+    // (exemple pour "c'était", seulement "était" est ajouté dans le tableau)
+    static String[] transformerTexteEnMots(String texte) {
+        String[] mots = new String[0];
+        // On met le texte en minuscules
+        texte = texte.toLowerCase();
+        // On concatène les caractères les uns à la suite des autre dans cette chaine de caractères
+        String mot = "";
+        // On parcours le texte caractère par caractère
+        for (int i = 0; i < texte.length(); i++) {
+            char c = texte.charAt(i);
+            // On enlève les accents des lettres et on concatène le caractère seulement si c'est une lettre
+            // On traite aussi les 2 caractères spéciaux "æ" et "œ"
+            // Source: https://fr.wikipedia.org/wiki/Diacritiques_utilis%C3%A9s_en_fran%C3%A7ais#:~:text=%C2%AB%20D%C3%A8s%20lors%20les%20voyelles%20et,%2D%20%C3%BC%20%2D%20%C3%BF%20%2D%20%C3%A7.
+            switch (c) {
+                case 'à', 'â', 'ä' ->
+                    c = 'a';
+                case 'é', 'è', 'ê', 'ë' ->
+                    c = 'e';
+                case 'î', 'ï' ->
+                    c = 'i';
+                case 'ô', 'ö' ->
+                    c = 'o';
+                case 'ù', 'û', 'ü' ->
+                    c = 'u';
+                case 'ÿ' ->
+                    c = 'y';
+                case 'ç' ->
+                    c = 'c';
+            }
+            if (c >= 'a' && c <= 'z') {
+                mot += c;
+            } else if (c == 'æ') {
+                mot += "ae";
+            } else if (c == 'œ') {
+                mot += "oe";
+            } else {
+                // On ajoute le mot seulement si le mot n'est pas déjà contenu dans le tableau et si ce n'est pas une lettre seule
+                // (exemple: le mot "c" ne sera pas ajouté dans le tableau puisque c'est une lettre seule)
+                if (mot.length() > 1 && !estContenuDansTableau(mot, mots)) {
+                    mots = ajouterPlaceTableau(mots);
+                    mots[mots.length - 1] = mot;
+                }
+                mot = "";
+            }
+        }
+        return mots;
+    }
+
+    // Retourne vrai si la chaine de caractères est contenu dans le tableau
+    // Retourne faux si elle n'y est pas
+    static boolean estContenuDansTableau(String search, String[] tableau) {
+        for (String str : tableau) {
+            if (str.equals(search)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Retourne un tableau au contenu identique à celui entré en paramètres mais avec un emplacement vide en dernier index
     static String[] ajouterPlaceTableau(String[] tableau) {
         String[] result = new String[tableau.length + 1];
         for (int i = 0; i < tableau.length; i++) {
