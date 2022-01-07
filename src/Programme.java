0778a5677aca1049d6a0199752b510a9a63a7013
FarmVivi <farmvividev@gmail.com>
2022-01-07 10:31:21 +0100

    comments

--- src/Programme.java
+++ src/Programme.java
@@ -457,6 +457,7 @@ public class Programme {
         for (int i = 0; i < TERMINAL_HEIGHT; i++) {
             System.out.println();
         }
+        // Scroll le terminal
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
